9ebbaf1ec70d59e384684bec38bc82fabe510519
Bijithajose <bijithajose55@gmail.com>
2025-05-28 19:35:35 +0530

    initial changes

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MathNet.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Math .NET Calculator.");
+        double a = 3;
+        double b = 4;
+
+        double result = SpecialFunctions.Hypotenuse(a, b);
+        Console.WriteLine($"Hypotenuse of {a}, {b} is: {result}");
+    }
+}
