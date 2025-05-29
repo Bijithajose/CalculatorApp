09d6c3af36f3177dfbf1066df80874b3feaf8f6e
Bijithajose <bijithajose55@gmail.com>
2025-05-29 08:29:54 +0530

    modified code

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -8,8 +8,11 @@ class Program
         Console.WriteLine("Math .NET Calculator.");
         double a = 3;
         double b = 4;
+        double c = 31.2112;
 
         double result = SpecialFunctions.Hypotenuse(a, b);
         Console.WriteLine($"Hypotenuse of {a}, {b} is: {result}");
+        double result1 = SpecialFunctions.Gamma(c);
+        Console.WriteLine($"Gamma of {c} is: {result1}");
     }
 }
